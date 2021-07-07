bda044256b3626e6b3de5fdbac7758bae9e7686b
Thiago Gonçalves Prudêncio <thiago.gprudencio@gmail.com>
2021-07-07 10:33:45 -0300

    test jitsiButtons

---
+++ jitsiButtons.php
@@ -0,0 +1,24 @@
+<?php
+
+    $userType = $_GET['userType'];
+
+    if ($userType == 'student') {
+        $toolbarButtons: array(
+            'microphone', 'camera', 'fullscreen',
+            'hangup', 'chat', 'raisehand',
+            'videoquality', 'feedback', 'stats',
+            'tileview', 'select-background', 'download'
+        );  
+    } else {
+        $toolbarButtons: array(
+            'microphone', 'camera', 'closedcaptions', 'desktop', 'embedmeeting', 'fullscreen',
+            'fodeviceselection', 'hangup', 'profile', 'chat', 'recording',
+            'livestreaming', 'etherpad', 'sharedvideo', 'shareaudio', 'settings', 'raisehand',
+            'videoquality', 'filmstrip', 'invite', 'feedback', 'stats', 'shortcuts',
+            'tileview', 'select-background', 'download', 'help', 'mute-everyone', 'mute-video-everyone', 'security'
+        );  
+    }
+
+    echo $toolbarButtons;
+
+?>
